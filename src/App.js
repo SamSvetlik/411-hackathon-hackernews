b2d5ebc2364bbbd184dfd2b585de909e41f95e5d
SamSvetlik <sam.svetlik@gmail.com>
2022-10-26 20:09:16 -0500

    sketched out a plan with HTML tags. App currently broken.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"  />
+         <h1>Search Hacker News</h1>
+         <form>
+          <input placeholder='Search stories by title, url, or author'></input>
+         </form>
+         <span>Settings</span>
       </header>
+      <main>
+        <div>
+          <h2>Filter by:</h2>
+          <form>
+            <input>dropdown menu with default, date, and author</input>
+          </form>
+          <h2>number of results</h2>
+        </div>
+        <ul>The search results will be listed here as LI's
+
+        </ul>
+      </main>
+      <footer>
+          There will be a bunch of a tags here for links
+      </footer>
     </div>
   );
 }
